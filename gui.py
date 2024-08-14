295bb5088f942490a93a8e0386fad194951f1b88
mE <metaescape@foxmail.com>
2024-08-15 00:39:51 +0800

    support fix duration recording

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+import time
+import subprocess, threading
+import sys
 
 TITLE = "ScreenX"
-
-import subprocess
 
 
 def notify_send(message, seconds=5):
@@ -18,6 +19,9 @@ class BorderLine(tk.Toplevel):
         self.overrideredirect(True)
         self.attributes("-topmost", True)
         self.config(bg="#bf616a")
+
+    def update_geometry(self, x, y, width, height):
+        self.geometry(f"{width}x{height}+{x}+{y}")
 
 
 class ScreenRecorderApp:
@@ -39,7 +43,8 @@ class ScreenRecorderApp:
     def init_root(self):
         self.root = tk.Tk()
         self.root.bind("<Escape>", self.exit_program)
-        self.recording = False
+        self.recording = ""
+        self.state = "normal"
 
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
@@ -67,51 +72,93 @@ class ScreenRecorderApp:
         self.buttons = {}
         self.borders = []
 
-    def on_button_press(self, event):
-        self.start_x = event.x
-        self.start_y = event.y
+    def update_bbox(self):
+        self.bbox = {
+            "top": self.root.winfo_y(),
+            "left": self.root.winfo_x(),
+            "width": self.root.winfo_width(),
+            "height": self.root.winfo_height(),
+        }
 
-        if self.selection_box:
-            self.canvas.delete(self.selection_box)
-        self.selection_box = self.canvas.create_rectangle(
-            self.start_x,
-            self.start_y,
-            self.start_x,
-            self.start_y,
-            outline="black",
-            width=2,
-        )
+    def on_button_press(self, event):
+        if self.state == "normal":
+            self.start_x = event.x
+            self.start_y = event.y
+
+            if self.selection_box:
+                self.canvas.delete(self.selection_box)
+            self.selection_box = self.canvas.create_rectangle(
+                self.start_x,
+                self.start_y,
+                self.start_x,
+                self.start_y,
+                outline="black",
+                width=2,
+            )
+        elif self.state == "focused":
+            self.button_window.attributes("-alpha", 0)
+            self.root.attributes("-alpha", 0.4)
+            self.x = event.x
+            self.y = event.y
 
     def on_mouse_drag(self, event):
-        self.canvas.coords(
-            self.selection_box, self.start_x, self.start_y, event.x, event.y
-        )
+        if self.state == "normal":
+            self.canvas.coords(
+                self.selection_box,
+                self.start_x,
+                self.start_y,
+                event.x,
+                event.y,
+            )
+        elif self.state == "focused":
+            deltax = event.x - self.x
+            deltay = event.y - self.y
+            x = self.root.winfo_x() + deltax
+            y = self.root.winfo_y() + deltay
+            self.root.geometry(f"+{x}+{y}")
 
     def on_button_release(self, event):
-        self.end_x = event.x
-        self.end_y = event.y
+        if self.state == "normal":
+            self.end_x = event.x
+            self.end_y = event.y
 
-        self.start_x = min(self.start_x, self.end_x)
-        self.start_y = min(self.start_y, self.end_y)
-        self.end_x = max(self.start_x, self.end_x)
-        self.end_y = max(self.start_y, self.end_y)
+            self.start_x = min(self.start_x, self.end_x)
+            self.start_y = min(self.start_y, self.end_y)
+            self.end_x = max(self.start_x, self.end_x)
+            self.end_y = max(self.start_y, self.end_y)
+            self.bbox["top"] = self.start_y
+            self.bbox["left"] = self.start_x
+            self.bbox["width"] = self.end_x - self.start_x
+            self.bbox["height"] = self.end_y - self.start_y
 
-        self.bbox["top"] = self.start_y
-        self.bbox["left"] = self.start_x
-        self.bbox["width"] = self.end_x - self.start_x
-        self.bbox["height"] = self.end_y - self.start_y
+            # 调整 root 窗口的大小和位置以匹配选择框
+            width = self.end_x - self.start_x + 4
+            height = self.end_y - self.start_y + 4
+            x = self.start_x - 2
+            y = self.start_y - 2
 
-        # 调整 root 窗口的大小和位置以匹配选择框
-        width = self.end_x - self.start_x + 4
-        height = self.end_y - self.start_y + 4
-        x = self.start_x - 2
-        y = self.start_y - 2
+            self.transparent_window_with_borders(x, y, width, height)
+            self.create_button_window()
 
-        self.transparent_window_with_borders(x, y, width, height)
+        elif self.state == "focused":
+            self.x = None
+            self.y = None
+            x, y = self.root.winfo_x(), self.root.winfo_y()
+            width, height = self.root.winfo_width(), self.root.winfo_height()
+            self.button_window.attributes("-alpha", 1.0)
+            self.end_x = x + width
+            self.start_y = y
 
-        self.create_button_window()
+            self.button_window.geometry(f"+{self.end_x}+{self.start_y}")
+
+            for border in self.borders:
+                border.destroy()
+
+            self.transparent_window_with_borders(x, y, width, height)
+            self.update_bbox()
 
     def transparent_window_with_borders(self, x, y, width, height):
+        self.state = "readonly"
         self.root.geometry(f"{width}x{height}+{x}+{y}")
         self.root.attributes("-alpha", 0)
 
@@ -137,6 +184,28 @@ class ScreenRecorderApp:
         )
         self.borders = [top_border, left_border, bottom_border, right_border]
 
+    def validate_input(self, value):
+        if value.isdigit() or value == "":
+            return True
+        else:
+            return False
+
+    def start_timer(self):
+        # 获取用户输入的秒数
+        input_value = self.input_area.get()
+        print(input_value)
+
+        if input_value.isdigit():
+            seconds = int(input_value)
+            # 启动后台线程来计时
+            threading.Thread(target=self.run_timer, args=(seconds,)).start()
+
+    def run_timer(self, seconds):
+        print(f"Timer started for {seconds} seconds.")
+        time.sleep(seconds)
+        self.toggle_recording(self.recording)
+        print("Timer ended, event is set.")
+
     def create_button_window(self):
         self.button_window = tk.Toplevel(self.root)
         self.button_window.overrideredirect(True)  # 去掉窗口边框
@@ -145,17 +214,24 @@ class ScreenRecorderApp:
         )  # 将按钮窗口放置在选择框的右侧
         self.button_window.attributes("-alpha", 1.0)  # 确保按钮窗口不透明
 
+        self.input_area = tk.Entry(
+            self.button_window, width=6, font=("Helvetica", 14)
+        )
+        self.input_area.pack(side=tk.TOP, padx=2, pady=2)
+        vcmd = (self.root.register(self.validate_input), "%P")
+        self.input_area.config(validate="key", validatecommand=vcmd)
+
         video_button = tk.Button(
             self.button_window,
             text="video",
-            command=self.toggle_video_recording,
+            command=lambda: self.toggle_recording("video"),
         )
         video_button.pack(side=tk.TOP, padx=5, pady=5)
 
         gif_button = tk.Button(
             self.button_window,
             text="gif",
-            command=self.toggle_gif_recording,
+            command=lambda: self.toggle_recording("gif"),
         )
         gif_button.pack(side=tk.TOP, padx=5, pady=5)
 
@@ -187,27 +263,20 @@ class ScreenRecorderApp:
         self.root.destroy()
         self.init_root()
 
-    def toggle_video_recording(self):
+    def toggle_recording(self, media="video"):
         if not self.recording:
-            self.recording = True
+            self.recording = media
+            self.start_timer()
             self.stop_event, *self.threads = self.start_video_hook()
-            self.buttons["video"].config(text="stop")
+            self.buttons[media].config(text="stop")
         else:
-            self.recording = False
-            self.buttons["video"].config(text="video")
+            self.recording = ""
+            self.buttons[media].config(text=media)
             self.stop_recording()
-            self.stop_video_hook()
-
-    def toggle_gif_recording(self):
-        if not self.recording:
-            self.recording = True
-            self.stop_event, *self.threads = self.start_gif_hook()
-            self.buttons["gif"].config(text="stop")
-        else:
-            self.recording = False
-            self.buttons["gif"].config(text="gif")
-            self.stop_recording()
-            self.stop_gif_hook()
+            if media == "video":
+                self.stop_video_hook()
+            elif media == "gif":
+                self.stop_gif_hook()
 
     def capture_image(self):
         self.capture_image_hook()
@@ -235,8 +304,13 @@ class ScreenRecorderApp:
         self.root.mainloop()
 
     def exit_program(self, event=None):
+        if self.stop_event:
+            self.stop_event.set()
+        for thread in self.threads:
+            if thread.is_alive():
+                thread.join()
         self.root.destroy()
-        exit(0)
+        sys.exit(0)
 
 
 # Example of how to use this selection in your recording
